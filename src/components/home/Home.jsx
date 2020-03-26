ef995ba41aaa076750065c32e84690e8885b3e65
PhillipKellogg <kelloggphillip@gmail.com>
2020-03-25 21:57:44 -0400

    Started Folder Structure

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Pages from "../pages/Pages"
+
+export default class Home extends Component
+{
+    render()
+    {
+        return (
+            <Pages>
+                <h1>Hello</h1>
+            </Pages>
+        );
+    }
+}
+
---
+++ src/components/pages/Pages.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+export default function (props)
+{
+    return (
+        <main className="main">
+            {/* <Header /> */}
+            {props.children}
+            {/* <Footer /> */}
+        </main>
+    );
+}
